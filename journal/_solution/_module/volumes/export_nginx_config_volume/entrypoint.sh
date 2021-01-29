d4f55d03122729f6867bc9b9a18bb1a1d64d989f
hvb <herman@vcweb.co>
2021-01-29 09:28:40 +0000

    updated streamsets

--- journal/_solution/_module/volumes/export_nginx_config_volume/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-envsubst < /vcw/pwd/urlrewrite.template > /vcw/pwd/urlrewrite.conf
-envsubst < /vcw/pwd/urlrewrite.template > /etc/nginx/conf.d/urlrewrite.conf
-nginx -g 'daemon off;'
